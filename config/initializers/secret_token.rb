6f4169dd361e2622d9e56b2e23041cefd94bd266
tradespring1 <github1@mailinator.com>
2012-10-17 02:43:25 -0700

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tradespring::Application.config.secret_token = '19005805bd0f6e6488904db3e87f5f48c2816925b6f84b2f1f0c21158e588d7072b5298a6e24f72cec2dda691cf75a442112370eec679cc17536a7b28feb04f0'
