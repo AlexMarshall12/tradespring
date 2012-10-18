e3c9c3ab57d84666f9579f49004a93faeb34596a
tradespring1 <marshall.a.x@gmail.com>
2012-10-17 23:50:29 -0700

    third

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,73 @@
 class UsersController < ApplicationController
-  def new
+  before_filter :signed_in_user,     only: [:index, :edit, :update, :destroy]
+  before_filter :correct_user,       only: [:edit, :update]
+  before_filter :admin_user,     only: :destroy
+
+  
+  def index
+    @users = User.all
   end
+
+  def show
+    @user = User.find(params[:id])
+    @purchases= @user.purchases
+    @sales= @user.sales
+    @purchase=Purchase.new
+    @sale=Sale.new
+  end
+
+  def new
+    @user = User.new
+    @title = "Sign up"
+  end
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      sign_in @user
+      flash[:success] = "Welcome to TradeSpring!"
+      redirect_to @user
+    else
+      @title = "Edit user"
+      render 'new'
+    end
+  end
+
+  def edit
+    @title = "Edit user"
+  end
+
+  def home 
+  	@user = User.find(params[:id])
+  	@purchases= @user.purchases
+  	@sales= @user.sales
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+ 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
+  end
+
+  private
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
 end
+
